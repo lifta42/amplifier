bee4c8fddd0181cd0a7cbd951427f39cb11750da
lifta42 <lifta42@outlook.com>
2017-12-11 14:35:47 +0800

    Make some trivial update while sleepy

--- src/dfa.h
+++ src/dfa.h
@@ -21,24 +21,37 @@ typedef int DFASymbol;
 
 typedef struct _DFAuto *DFAuto;  // all-capital seems weird to me
 
-/// Create a new DFA with no state.
-/// Invoking `dfa_work` directly on its result will cause error.
-/// \return An empty DFA
+/** Create a new DFA with no state.
+
+Invoking `dfa_work` directly on its result will cause error.
+
+\return An empty DFA. */
 DFAuto dfa_create();
 
-/// Create a new state node in a DFA.
-/// \param dfa the extended DFA.
-/// \return The state that created for `dfa`
+/** Create a new state node in a DFA.
+
+\param dfa the extended DFA
+\return The state that created for `dfa`. */
 DFAState dfa_extend(DFAuto dfa);
 
-/// Draw an arrow between two states of a DFA.
-/// After this invoking, `dfa` will jump from `from` state to `to` state when
-/// `with` symbol is received.
-/// Connecting states that not exists will cause error.
-/// \param dfa the modified DFA.
-/// \param from the source state of jumping.
-/// \param with the symbol that causes jumping.
-/// \param to the destined state of jumping.
+/** Draw an arrow between two states of a DFA.
+After this invoking, `dfa` will jump from `from` state to `to` state when `with`
+symbol is received.
+
+Connecting states that not exists will cause error.
+
+\param dfa the modified DFA
+\param from the source state of jumping
+\param with the symbol that causes jumping
+\param to the destined state of jumping */
 void dfa_connect(DFAuto dfa, DFAState from, DFASymbol with, DFAState to);
+
+/** Similiar to `dfa_connect`, only the jumping will occur if none of any
+existing symbol matches the received one.
+
+\param dfa the modified DFA
+\param from the source state of jumping
+\param to the destined state of jumping */
+void dfa_connect_fallback(DFAuto dfa, DFAState from, DFAState to);
 
 #endif
