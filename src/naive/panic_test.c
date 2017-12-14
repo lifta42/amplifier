303bac45696406b2dc180a95540b0394af2e0d3e
lifta42 <lifta42@outlook.com>
2017-12-14 10:13:13 +0800

    Rewrite test for naive-panic with sandbox

--- src/naive/panic_test.c
+++ src/naive/panic_test.c
@@ -1,5 +1,6 @@
 // Created by liftA42 on Dec 13, 2017.
 #include "panic.h"
+#include "sand.h"
 
 #include <assert.h>
 #include <errno.h>
@@ -11,56 +12,23 @@
 #include <unistd.h>
 #endif
 
+int panic_error(void *payload)
+{
+  panic(42 == 43, "this one should fail with a greeting: %s", "hello!");
+  return 0;  // trivial
+}
+
 int main()
 {
   {
     panic(42 == 42, "the world is running in a wrong way");
   }
-
-#ifndef _WIN32
   {
-    // http://www.microhowto.info/howto/capture_the_output_of_a_child_process_in_c.html
-    int filedes[2];
-    pipe(filedes);
-    pid_t pid = fork();
-
-    if (pid == 0)
-    {
-      while ((dup2(filedes[1], STDERR_FILENO) == -1) && (errno == EINTR))
-      {
-        // nothing, just waiting
-      }
-      close(filedes[1]);
-      close(filedes[0]);
-      panic(42 == 43, "this one should fail with a greeting: %s", "hello!");
-    }
-
-    int status;
-    waitpid(pid, &status, 0);
-    assert(WEXITSTATUS(status) == 1);
-
-    close(filedes[1]);
+    Sandbox box = sand_create(panic_error);
+    assert(sand_run(box, NULL) == 1);
     char buffer[64];
-    while (true)
-    {
-      ssize_t count = read(filedes[0], buffer, sizeof(buffer));
-      if (count == -1)
-      {
-        assert(errno == EINTR);
-        continue;
-      }
-      else
-      {
-        break;
-      }
-    }
-    close(filedes[0]);
+    sand_read(box, 1, buffer, sizeof(buffer));
     assert(strcmp(buffer, "this one should fail with a greeting: hello!\n")
            == 0);
   }
-#else
-  {
-    // TODO
-  }
-#endif
 }
